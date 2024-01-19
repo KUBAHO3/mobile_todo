8eb1c560076a5b3fe1efe94c463a48759066f8b8
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2024-01-19 08:30:15 +0200

    Display a list of todos

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  const [todos, setTodos] = useState([
+    { text: 'My todo no1', key: '1'},
+    { text: 'My todo no2', key: '2'},
+    { text: 'My todo no3', key: '3'},
+  ])
+
+  return(
+  <View style={styles.container}>
+    <View style={styles.content}>
+      <View style={styles.list}>
+        <FlatList data={todos} renderItem={({ item }) => (<Text>{item.text}</Text>)} />
+      </View>
     </View>
-  );
-}
+  </View>
+  )
+ }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {    
+    padding: 40,
+  },
+  list: {
+    marginTop: 12,
+  }
 });
+
+export default App;
