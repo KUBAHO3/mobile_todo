5b670cdbcca42de2c4415a6bd2820d3024d036f8
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2024-01-19 13:46:30 +0200

    Add the alert and validations

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
-import { Button, StyleSheet, TextInput, View } from 'react-native'
+import { Alert, Button, StyleSheet, TextInput, View } from 'react-native'
 
 function AddTodo({AddHandler}) {
-  const [ todo, setTodo ] = useState(undefined)
+  const [ todo, setTodo ] = useState('')
 
   const changeHandler = (val) => {
     setTodo(val)
   }
 
   const toAddHandler = (val) => {
-    AddHandler(val)
-    setTodo(undefined)
+    if(val.length > 3){
+        AddHandler(val)
+        setTodo('')
+    } else {
+        Alert.alert('Aleee Babu', 'Atleast 4 chars required', [{text: 'GOT IT', onPress: ()=> console.log('Alert closed')}])
+    }
   }
 
   return (
